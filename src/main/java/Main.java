4ac1a5cb2c0bd181e486d72c82086c93c474d260
eduardoxis <134206114+eduardoxis@users.noreply.github.com>
2024-08-26 15:57:14 -0300

    aula 26/08/24

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class main {
+import model.Pagamento;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pagamento pg = new Pagamento();
+        pg.setId(11);
+        pg.setValor(500);
+        pg.setData(new Date());
+        System.out.println(pg);
+
+
+
+
+    }
+
 }
--- src/main/java/model/Pagamento.java
+++ src/main/java/model/Pagamento.java
@@ -1,2 +1,43 @@
-package model;public class Pagamento {
+package model;
+
+import java.util.Date;
+
+public class Pagamento {
+
+    private Date data;
+    private float valor;
+    private long id;
+
+    @Override
+    public String toString() {
+        return "Pagamento{" +
+                "data=" + data +
+                ", valor=" + valor +
+                ", id=" + id +
+                '}';
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
